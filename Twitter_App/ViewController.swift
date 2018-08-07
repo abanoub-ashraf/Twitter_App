c4eba130447209b9637b122d893f97aa1a2e2bf9
Abanoub Ashraf <mrbob539@gmail.com>
2018-08-08 01:08:55 +0200

    initial commit

--- Twitter_App/ViewController.swift
+++ Twitter_App/ViewController.swift
@@ -1,25 +1,13 @@
-//
-//  ViewController.swift
-//  Twitter_App
-//
-//  Created by Abanoub Ashraf on 8/8/18.
-//  Copyright © 2018 Abanoub Ashraf. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
 }
-
